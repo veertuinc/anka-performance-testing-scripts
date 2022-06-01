5e0bdcc17183cd578eccea1eb19d51937041d848
Nathan Pierce <norsegaud@gmail.com>
2022-06-01 08:33:42 -0400

    test

--- dogecoin.bash
+++ dogecoin.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -eo pipefail
-brew install automake libtool boost miniupnpc openssl pkg-config protobuf qt5 libevent berkeley-db
-git clone https://github.com/dogecoin/dogecoin -b 1.14-maint
-cd /Users/anka/dogecoin
-export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-export CXXFLAGS="-I/usr/local/opt/openssl@1.1/include"
-./autogen.sh
-./configure --disable-wallet --without-gui
-[[ -z "${1}" ]] && THREADS=12 || THREADS="${1}"
-time make -j "${THREADS}"
+# brew install automake libtool boost miniupnpc openssl pkg-config protobuf qt5 libevent berkeley-db
+# git clone https://github.com/dogecoin/dogecoin -b 1.14-maint
+# cd /Users/anka/dogecoin
+# export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+# export CXXFLAGS="-I/usr/local/opt/openssl@1.1/include"
+# ./autogen.sh
+# ./configure --disable-wallet --without-gui
+# [[ -z "${1}" ]] && THREADS=12 || THREADS="${1}"
+# make -j "${THREADS}"
+echo 123
--- webkit.bash
+++ webkit.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-# git clone https://github.com/WebKit/WebKit.git WebKit This would take forever
-cd WebKit
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-time ./Tools/Scripts/build-webkit --release
+echo 342
+# git clone https://github.com/WebKit/WebKit.git # WebKit This would take forever
+# cd WebKit
+# sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+# ./Tools/Scripts/build-webkit --release

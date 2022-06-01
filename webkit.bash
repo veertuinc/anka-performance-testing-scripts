bebcdee15283e72fd8bc01d8a475cd75a6fdff22
Nathan Pierce <norsegaud@gmail.com>
2022-06-01 18:02:57 -0400

    quick fix

--- webkit.bash
+++ webkit.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-echo 342
-# git clone https://github.com/WebKit/WebKit.git # WebKit This would take forever
-# cd WebKit
-# sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-# ./Tools/Scripts/build-webkit --release
+git clone https://github.com/WebKit/WebKit.git # WebKit This would take forever
+cd WebKit
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+./Tools/Scripts/build-webkit --release

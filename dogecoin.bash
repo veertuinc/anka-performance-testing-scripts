5cee385da273408b356ba954a4bed6668109e890
Nathan Pierce <norsegaud@gmail.com>
2022-06-10 10:50:13 -0400

    quick fix

--- dogecoin.bash
+++ dogecoin.bash
@@ -4,14 +4,17 @@ if [[ $(sw_vers) =~ 10\.14 ]]; then
   >&2 echo "10.14 has issues with JDK being needed and brew will not install required packages properly... skipping" 
   exit 1
 fi
-brew install automake libtool boost miniupnpc openssl pkg-config protobuf qt5 libevent berkeley-db
-git clone https://github.com/dogecoin/dogecoin -b 1.14-maint
-cd /Users/anka/dogecoin
-export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-export CXXFLAGS="-I/usr/local/opt/openssl@1.1/include"
-./autogen.sh
-./configure --disable-wallet --without-gui
-[[ -z "${1}" ]] && THREADS=2 || THREADS="${1}"
-make -j "${THREADS}"
-
+if [[ "${1}" == "prep" ]]; then
+  brew install automake libtool boost miniupnpc openssl pkg-config protobuf qt5 libevent berkeley-db
+  git clone https://github.com/dogecoin/dogecoin -b 1.14-maint
+fi
+if [[ "${1}" == "build" ]]; then
+  cd /Users/anka/dogecoin
+  export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+  export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+  export CXXFLAGS="-I/usr/local/opt/openssl@1.1/include"
+  ./autogen.sh
+  ./configure --disable-wallet --without-gui
+  [[ -z "${1}" ]] && THREADS=2 || THREADS="${1}"
+  make -j "${THREADS}"
+fi
--- ffmpeg.bash
+++ ffmpeg.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -exo pipefail
-git clone https://git.ffmpeg.org/ffmpeg.git
-cd ffmpeg
-git checkout n4.0
-./configure --disable-autodetect --disable-asm
-[[ -z "${1}" ]] && THREADS=2 || THREADS="${1}"
-make -j "${THREADS}"
+if [[ "${1}" == "prep" ]]; then
+  git clone https://git.ffmpeg.org/ffmpeg.git
+fi
+if [[ "${1}" == "build" ]]; then
+  cd ffmpeg
+  git checkout n4.0
+  ./configure --disable-autodetect --disable-asm
+  [[ -z "${1}" ]] && THREADS=2 || THREADS="${1}"
+  make -j "${THREADS}"
+fi

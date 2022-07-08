9686f65d6718c8c905dfe6e9fa9f72d615cec083
Nathan Pierce <norsegaud@gmail.com>
2022-07-08 17:51:17 -0400

    swift-voxel simulator test

---
+++ swift-voxel.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -exo pipefail
+if [[ "${*}" =~ "prep" ]]; then
+  [[ ! -d SwiftVoxel ]] && git clone https://github.com/claygarrett/SwiftVoxel.git
+fi
+if [[ "${*}" =~ "build" ]]; then
+  xcodebuild -workspace SwiftVoxel.xcworkspace -scheme SwiftVoxel -destination 'platform=iOS Simulator,name=iPhone 13 Pro,OS=15.5' build
+  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.iPhone-13-Pro" com.apple.CoreSimulator.SimRuntime.iOS-15-5)
+  xcron simctl boot "${SIMID}"
+  sleep 20
+  open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+  xcrun simctl install test /tmp/Build/Products/Debug-iphonesimulator/SwiftVoxel.app
+  BUNDLE_ID="$(defaults read /tmp/Build/Products/Debug-iphonesimulator/SwiftVoxel.app/Info.plist CFBundleIdentifier)"
+  xcrun simctl launch test "${BUNDLE_ID}"
+  sleep 300 # Sleep 5 minutes to make sure the VM doesn't crash
+fi

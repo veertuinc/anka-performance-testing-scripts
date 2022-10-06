f3a83f27eb2279fa3ba21d36bab49359df65f8d9
Nathan Pierce <norsegaud@gmail.com>
2022-10-06 13:05:36 -0400

    get latest pro simulator

--- swift-voxel.bash
+++ swift-voxel.bash
@@ -6,8 +6,9 @@ if [[ "${*}" =~ "prep" ]]; then
 fi
 if [[ "${*}" =~ "build" ]]; then
   cd SwiftVoxel
-  xcodebuild -workspace SwiftVoxel.xcworkspace -derivedDataPath /tmp/ -scheme SwiftVoxel -destination 'platform=iOS Simulator,name=iPhone 13 Pro' build
-  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.iPhone-13-Pro")
+  SIM_VER="$(xcrun simctl list --json devices available | grep name | grep Pro | head -1 | cut -d\" -f 4)"
+  xcodebuild -workspace SwiftVoxel.xcworkspace -derivedDataPath /tmp/ -scheme SwiftVoxel -destination "platform=iOS Simulator,name=${SIM_VER}" build
+  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.$(echo ${SIM_VER} | sed 's/ /-/g')")
   xcrun simctl boot "${SIMID}"
   sleep 120
   open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app

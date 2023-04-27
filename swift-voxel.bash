24cd0186b8ce3d8ec1aff382cd88040e239c4ebc
Nathan Pierce <norsegaud@gmail.com>
2023-04-27 10:59:08 -0400

    test

--- swift-voxel.bash
+++ swift-voxel.bash
@@ -7,9 +7,11 @@ fi
 if [[ "${*}" =~ "build" ]]; then
   cd SwiftVoxel
   xcrun simctl list --json devices available; sleep 20 # fix a weird bug where xcrun simctl list --json devices available is empty the first run
-  SIM_VER="$(xcrun simctl list --json devices available | grep name | grep Pro | head -1 | cut -d'"' -f4)"
+  # SIM_VER="$(xcrun simctl list --json devices available | grep name | grep Pro | head -1 | cut -d'"' -f4)"
+  SIM_VER="iPhone 11 Pro"
   xcodebuild -workspace SwiftVoxel.xcworkspace -derivedDataPath /tmp/ -scheme SwiftVoxel -destination "platform=iOS Simulator,name=${SIM_VER}" build
-  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.$(echo ${SIM_VER} | sed 's/ /-/g')")
+  # SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.$(echo ${SIM_VER} | sed 's/ /-/g')")
+  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.iPhone-11-Pro")
   xcrun simctl boot "${SIMID}"
   sleep 120
   open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app

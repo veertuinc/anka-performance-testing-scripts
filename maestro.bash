c2e47124bb3e155380b466e664570eee3387849d
Nathan Pierce <norsegaud@gmail.com>
2024-10-29 13:02:14 -0500

    testing new maestro tests

---
+++ maestro.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -exo pipefail
+cd "${HOME}"
+if [[ "${*}" =~ "prep" ]]; then
+  brew install xcodesorg/made/xcodes
+  xcodes runtimes install "iOS 17.5"
+fi
+if [[ "${*}" =~ "build" ]]; then
+  # DEVICE_ID=$(sh -c "xcrun simctl create 'iPhone 15 Pro Max' 'iPhone 15 Pro Max' 'com.apple.CoreSimulator.SimRuntime.iOS-17-5'" | tr -d '\r')
+  # xcrun --verbose simctl boot ${DEVICE_ID}
+  xcrun simctl list --json devices available; sleep 20 # fix a weird bug where xcrun simctl list --json devices available is empty the first run
+  SIM_VER="$(xcrun simctl list --json devices available | grep name | grep Pro | head -1 | cut -d'"' -f4)"
+  SIMID=$(xcrun simctl create test "com.apple.CoreSimulator.SimDeviceType.$(echo ${SIM_VER} | sed 's/ /-/g')")
+  xcrun simctl boot "${SIMID}"
+  sleep 120
+  maestro download-samples
+  cd ./samples; unzip sample.zip
+  xcrun --verbose simctl install Booted Wikipedia.app
+  export MAESTRO_DRIVER_STARTUP_TIMEOUT=60000000
+  maestro --verbose test ios-flow.yaml
+fi
